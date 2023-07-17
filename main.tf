886d48fbc428cbef00f1a78bc4f39f8fb3d535f8
devbyrahami <rahami321@gmail.com>
2023-07-17 18:33:13 +0800

    update back creds

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-southeast-1"  
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "app_server" {
   ami           = "ami-0f74c08b8b5effa56" # ap-southeast-1
   instance_type = "t2.micro"
   tags = {
