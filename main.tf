eb76d5997f2e4697fc9da2d79ab287242381b1cc
devbyrahami <rahami321@gmail.com>
2023-07-17 18:38:40 +0800

    update back creds

--- main.tf
+++ main.tf
@@ -1,11 +1,42 @@
-provider "aws" {
-    region = "ap-southeast-1"  
+# provider "aws" {
+#     region = "ap-southeast-1"  
+# }
+
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-0f74c08b8b5effa56" # ap-southeast-1
+#   instance_type = "t2.micro"
+#   tags = {
+#       Name = "TF-Jenkins"
+#   }
+# }
+
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
+
+//tell which provider. example: aws, azure etc..
+provider "aws" {
+  region  = "ap-southeast-1"
+}
+
+
+//Create instance one
 resource "aws_instance" "app_server" {
-  ami           = "ami-0f74c08b8b5effa56" # ap-southeast-1
+#   ami           = "ami-830c94e3"  ami-0f74c08b8b5effa56
+  ami           = "ami-0f74c08b8b5effa56" //AMI depends on which region
   instance_type = "t2.micro"
+
   tags = {
-      Name = "TF-Jenkins"
+    Name = "TF-Jenkins"
   }
 }
